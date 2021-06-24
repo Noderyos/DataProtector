f6b076f4edda8ca33fe7afd5376a105dd329c2c1
Noderyos <56077132+Noderyos@users.noreply.github.com>
2021-06-24 19:50:48 +0200

    Add files via upload

---
+++ Encodeur-GUI.py
@@ -0,0 +1,30 @@
+import os
+from hashlib import *
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+Tk().withdraw()
+file_input = askopenfilename()
+key = sha256(input("Mot de passe : ").encode('utf-8')).digest()
+if ".ncrypt" in file_input:
+    with open(file_input, 'rb') as f_input:
+        with open(file_input.replace(".ncrypt",""),"wb") as f_output:
+            i = 0
+            while f_input.peek():
+                c = ord(f_input.read(1))
+                j = i % len(key)
+                b = bytes([c^key[j]])
+                f_output.write(b)
+                i = i + 1
+    print("Fichier Décodé")
+else:
+    with open(file_input, 'rb') as f_input:
+        with open(file_input + ".ncrypt","wb") as f_output:
+            i = 0
+            while f_input.peek():
+                c = ord(f_input.read(1))
+                j = i % len(key)
+                b = bytes([c^key[j]])
+                f_output.write(b)
+                i = i + 1
+    print("Fichier Encodé")
+os.remove(file_input)
